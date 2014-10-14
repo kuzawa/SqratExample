4bba0f0f31139ba5c6770c64ccec2e1c25a5460c
kuzawa <yuusuke.kuzawa@gmail.com>
2014-10-14 20:32:23 +0900

    empty project

---
+++ SqratSample/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  SqratSample
+//
+//  Created by kuzawa on 2014/10/14.
+//  Copyright (c) 2014年 kuzawa. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
+
